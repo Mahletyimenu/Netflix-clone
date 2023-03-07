a7e730e57215e1d27990f34bb83106dcd5d2cb33
Mahlet <mahletyimenu92@gmail.com>
2023-03-07 19:22:56 +0300

    nitflix clone

--- src/request.js
+++ src/request.js
@@ -1,5 +1,5 @@
-const API_KEY ="b80a1ae744c1fb06f78932535017946d";
-//const API_KEY =process.env.REACT_APP_API_KEY;
+const API_KEY =process.env.REACT_APP_API_KEY;
+
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
